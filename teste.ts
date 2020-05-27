b992bbc0f762e8127ff3210baee989e9a2cfb42b
Lucas Vieira <lucasvieiraa@hotmail.com.br>
2020-05-27 15:49:15 -0300

    BootCamp Fundamentos ReactJS

---
+++ teste.ts
@@ -0,0 +1,12 @@
+<tr>
+                <td className="title">Computer</td>
+                <td className="income">R$ 5.000,00</td>
+                <td>Sell</td>
+                <td>20/04/2020</td>
+              </tr>
+              <tr>
+                <td className="title">Website Hosting</td>
+                <td className="outcome">- R$ 1.000,00</td>
+                <td>Hosting</td>
+                <td>19/04/2020</td>
+              </tr>
